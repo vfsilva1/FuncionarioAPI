bf24fe6001eb401bde59dcf30fd3c6381a34ee64
Vitor Silva <vfsilva5@gmail.com>
2020-03-03 07:03:43 -0300

    Adicionar arquivos de projeto.

---
+++ FuncionarioApp/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuncionarioApp
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public enum Sexo { MASCULINO, FEMININO };
+        public int PIS { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+        public string Email { get; set; }
+        public DateTime DataAdmissao { get; set; }
+    }
+}
